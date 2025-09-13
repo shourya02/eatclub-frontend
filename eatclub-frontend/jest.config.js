ecd45a4442501cf9ff61bbdda5a5f81821c69d82
Shourya <s3929838@student.rmit.edu.au>
2025-09-13 10:11:02 +1000

    added the test setup

---
+++ eatclub-frontend/jest.config.js
@@ -0,0 +1,21 @@
+// jest.config.js
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jsdom',
+  moduleNameMapping: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/app/api/route.ts',
+  ],
+};
+
+module.exports = createJestConfig(customJestConfig);
